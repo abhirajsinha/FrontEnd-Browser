22b22bb6dbf0dba60cc8298b5b8252f9b6e38df4
Abhiraj Sinha <abhirajsinha25@gmail.com>
2022-08-30 14:47:49 +0530

    Facing Issue in Functionalities

--- Jira Ticket Manager/script.js
+++ Jira Ticket Manager/script.js
@@ -1,11 +1,30 @@
 let modalCont = document.querySelector(".modal-cont");
 let mainCont = document.querySelector(".main-cont");
 let addBtn = document.querySelector(".action-add");
+let removeBtn = document.querySelector(".action-rem");
 let textareaCont = document.querySelector(".textarea-cont");
 let idCont = document.querySelector(".ticket-id");
 let taskareaCont = document.querySelector(".task-area");
+let allPriorityColors = document.querySelectorAll(".priority-color");
+let colors = ["green","blue","orange","red"];
+let modalPriorityColor = colors[colors.length-1];
 let addFlag = false;
+let removeFlag = false;
+let lockClass = "fa-lock";
+let unlockClass = "fa-lock-open";
 
+
+
+//Listener for modal Priority Coloring - SetBorder 
+allPriorityColors.forEach((colorElem, idx) =>{
+    colorElem.addEventListener("click",(e)=>{
+        allPriorityColors.forEach((priorityColorElem, idx) =>{
+            priorityColorElem.classList.remove("border");
+        })
+        colorElem.classList.add("border");
+        modalPriorityColor = colorElem.classList[0];
+    })
+})
 
 addBtn.addEventListener("click",(e)=>{
     // console.log("Add");
@@ -19,27 +38,54 @@ addBtn.addEventListener("click",(e)=>{
     }
 })
 
+removeBtn.addEventListener("click",(e)=>{
+    removeFlag = !removeFlag;
+})
+
 modalCont.addEventListener("keydown", (e) => {
     let key = e.key;
     if(key==="Shift"){
-        createTicket();
+        createTicket(modalPriorityColor, textareaCont.value, shortid());
         modalCont.style.display = "none";
         addFlag=false;
         textareaCont.value="";
     }
 })
 
-function createTicket(){
+function createTicket(ticketColor, ticketTask, ticketID){
     let ticketCont = document.createElement("div");
     ticketCont.setAttribute("class","ticket-cont");
     ticketCont.innerHTML = `
-            <div class="ticket-color"></div>
-            <div class="ticket-id">#12</div>
-            <div class="task-area">
-                Sample Text
-            </div>
+            <div class="ticket-color ${ticketColor}"></div>
+            <div class="ticket-id">#${ticketID}</div>
+            <div class="task-area">${ticketTask}</div>
+            <div class="ticket-lock">
+                    <i class="fa-solid fa-lock"></i>
+                </div>
     `;
     mainCont.appendChild(ticketCont);
-   
+    handleRemoval(ticketCont);
+    handleLock(ticketCont);
+}
+
+function handleRemoval(ticket){
+    // removeFlag -> true -> removeticket
+    if(removeFlag){
+        ticket.remove();
+    }
+}
+
+function handleLock(ticket){
+    let ticketLockElem = ticket.querySelector(".ticket-lock");
+    let ticketLock = ticketLockElem.children[0];
+    ticketLock.addEventListener("click",(e)=>{
+        if(ticketLock.classList.contains(lockClass)){
+            ticketLock.classList.remove(lockClass);
+            ticket.classList.add(unlockClass);
+        }else{
+            ticketLock.classList.remove(unlockClass);
+            ticket.classList.add(lockClass);
+        }
+    })
 }
 
