9ff73d034eeaaa984e893f33c68c0aadf3dc14b3
Abhiraj Sinha <abhirajsinha25@gmail.com>
2022-09-10 20:34:48 +0530

    Updated Modal Plus Button Settings

--- Jira Ticket Manager/script.js
+++ Jira Ticket Manager/script.js
@@ -1,9 +1,12 @@
 let colorBtn = document.querySelectorAll(".filter-color");
 let mainContainer = document.querySelector(".main-container");
+let modalContainer = document.querySelector(".modal-container");
 let body = document.body;
-let plusButton = document.querySelector(".fa-plus");
-let removeButton = document.querySelector(".fa-solid fa-xmark");
-let main_container = document.querySelector("main-container");
+let plusButton = document.querySelector(".add");
+let removeButton = document.querySelector(".remove");
+let modalFilterCont =  document.querySelector(".modal-filter-container");
+let flag = false;
+
 for (let i = 0; i < colorBtn.length; i++) {
   colorBtn[i].addEventListener("click", function (e) {
     let color = colorBtn[i].classList[1];
@@ -11,21 +14,12 @@ for (let i = 0; i < colorBtn.length; i++) {
   });
 }
 
-// 
+//Add Button
 plusButton.addEventListener("click", function () {
-  let div = document.createElement("div");
-  div.setAttribute("class", "modal-container");
-  div.innerHTML = `
-    <div class="input-container">
-    <textarea class="modal-input" placeholder="Enter Your Text Here"></textarea>
-  </div>
-  <div class="modal-filter-container">
-    <div class="filter lightpink"></div>
-    <div class="filter lightblue"></div>
-    <div class="filter lightgreen"></div>
-    <div class="filter black"></div>
-  </div>
-    `;
-    mainContainer.appendChild(div);
+  if (flag === false) {
+    modalContainer.style.display = "flex";
+  } else {
+    modalContainer.style.display = "none";
+  }
+  flag = !flag;
 });
-
