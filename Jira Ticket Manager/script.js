bffbc145504a460be08ae7c434ff30e067fd7716
Abhiraj Sinha <abhirajsinha25@gmail.com>
2022-09-10 20:19:10 +0530

    Added shadow in container

--- Jira Ticket Manager/script.js
+++ Jira Ticket Manager/script.js
@@ -1,8 +1,31 @@
 let colorBtn = document.querySelectorAll(".filter-color");
 let mainContainer = document.querySelector(".main-container");
+let body = document.body;
+let plusButton = document.querySelector(".fa-plus");
+let removeButton = document.querySelector(".fa-solid fa-xmark");
+let main_container = document.querySelector("main-container");
 for (let i = 0; i < colorBtn.length; i++) {
   colorBtn[i].addEventListener("click", function (e) {
     let color = colorBtn[i].classList[1];
     mainContainer.style.backgroundColor = color;
   });
 }
+
+// 
+plusButton.addEventListener("click", function () {
+  let div = document.createElement("div");
+  div.setAttribute("class", "modal-container");
+  div.innerHTML = `
+    <div class="input-container">
+    <textarea class="modal-input" placeholder="Enter Your Text Here"></textarea>
+  </div>
+  <div class="modal-filter-container">
+    <div class="filter lightpink"></div>
+    <div class="filter lightblue"></div>
+    <div class="filter lightgreen"></div>
+    <div class="filter black"></div>
+  </div>
+    `;
+    mainContainer.appendChild(div);
+});
+
