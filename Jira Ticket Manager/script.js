a30ef2365da9ba02ac5d43156df7c137e9106d9d
Abhiraj Sinha <abhirajsinha25@gmail.com>
2022-09-11 19:45:46 +0530

    Added Border on filters

--- Jira Ticket Manager/script.js
+++ Jira Ticket Manager/script.js
@@ -1,10 +1,9 @@
 let colorBtn = document.querySelectorAll(".filter-color");
 let mainContainer = document.querySelector(".main-container");
-let modalContainer = document.querySelector(".modal-container");
+// let modalContainer = document.querySelector(".modal-container");
 let body = document.body;
 let plusButton = document.querySelector(".add");
 let removeButton = document.querySelector(".remove");
-let modalFilterCont =  document.querySelector(".modal-filter-container");
 let flag = false;
 
 for (let i = 0; i < colorBtn.length; i++) {
@@ -16,10 +15,33 @@ for (let i = 0; i < colorBtn.length; i++) {
 
 //Add Button
 plusButton.addEventListener("click", function () {
-  if (flag === false) {
-    modalContainer.style.display = "flex";
-  } else {
-    modalContainer.style.display = "none";
+  let div = document.createElement("div");
+  div.setAttribute("class", "modal-container");
+  div.innerHTML = `
+      <div class="input-container">
+      <textarea class="modal-input" placeholder="Enter Your Text Here"></textarea>
+    </div>
+    <div class="modal-filter-container">
+      <div class="filter lightpink"></div>
+      <div class="filter lightblue"></div>
+      <div class="filter lightgreen"></div>
+      <div class="filter black"></div>
+    </div>
+      `;
+  mainContainer.appendChild(div);
+  let modalFilters = document.querySelectorAll(
+    ".modal-filter-container .filter"
+  );
+  //SetAttributes remove previous classes
+  //modalFilters[0].setAttribute("class",".border");
+
+  modalFilters[0].classList.add("border");
+  for (let i = 0; i < modalFilters.length; i++) {
+    modalFilters[i].addEventListener("click", function () {
+      modalFilters.forEach((filter) => {
+        filter.classList.remove("border");
+      });
+      modalFilters[i].classList.add("border");
+    });
   }
-  flag = !flag;
 });
